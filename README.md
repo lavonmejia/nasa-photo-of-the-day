feeaed65344173a3606707cd195a5ab6f8b924d9
LaVon Mejia <lavonmejia@gmail.com>
2019-07-17 20:05:42 -0400

    setting up container

--- README.md
+++ README.md
@@ -27,9 +27,9 @@ This project was put together using create-react-app (CRA). You will not need to
 - [X] Clone your OWN version of the repository in your terminal
 - [X] Download project dependencies by running one of these two commands `yarn` or `npm install`
 - [x] Using the same command tool (yarn or npm) start up the app using `yarn start` or `npm start`
-- [ ] Create a new branch: git checkout -b `<firstName-lastName>`.
+- [X] Create a new branch: git checkout -b `<firstName-lastName>`.
       Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
-- [ ] Push commits: git push origin `<firstName-lastName>`.
+- [X] Push commits: git push origin `<firstName-lastName>`.
 
 Follow these steps for completing your project.
 
--- src/App.js
+++ src/App.js
@@ -2,13 +2,59 @@ import React from "react";
 import "./App.css";
 
 function App() {
+
+// USEeffect HERE
+
   return (
+    
+    <div className="Container" >
+
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
     </div>
+
+      
+    <div className="TitleDiv">
+      <p>
+        Read through the instructions in the README.md file to build your NASA
+        app! Have fun 🚀!
+      </p>
+    </div>
+
+    <div className="PhotoDiv">
+      <p>
+        Read through the instructions in the README.md file to build your NASA
+        app! Have fun 🚀!
+      </p>
+    </div>
+
+    <div className="CaptionDiv">
+      <p>
+        Read through the instructions in the README.md file to build your NASA
+        app! Have fun 🚀!
+      </p>
+    </div>
+
+    <div className="DateDiv">
+      <p>
+        Read through the instructions in the README.md file to build your NASA
+        app! Have fun 🚀!
+      </p>
+    </div>
+
+    <div className="CentroidDiv">
+      <p>
+        Read through the instructions in the README.md file to build your NASA
+        app! Have fun 🚀!
+      </p>
+    </div>
+
+    </div>
+
+
   );
 }
 
